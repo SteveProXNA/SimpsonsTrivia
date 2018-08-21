90979637cf08f085265144f616cea00493f398a1
SteveProXNA <steven_boland@hotmail.com>
2018-08-21 20:23:00 +0100

    Upload all files

--- SimpsonsTrivia.XNA/SimpsonsTrivia.XNA.SystemTests/BaseSystemTests.cs
+++ SimpsonsTrivia.XNA/SimpsonsTrivia.XNA.SystemTests/BaseSystemTests.cs
@@ -28,9 +28,9 @@ namespace WindowsGame.SystemTests
 		protected IFileManager FileManager;
 		protected ILogger Logger;
 
-		// mklink /D E:\SimpsonsTrivia.XNA.Content  E:\GitHub\StevePro7\SimpsonsTriviaXNA\SimpsonsTrivia.XNA\SimpsonsTrivia.XNA\bin\x86\Debug\
-		// mklink /D E:\SimpsonsTrivia.XNA.Content  E:\SVN\SimpsonsTrivia\SimpsonsTrivia.XNA\SimpsonsTrivia.XNA\SimpsonsTrivia.XNA\bin\x86\Debug\
-		protected const String CONTENT_ROOT = @"E:\SimpsonsTrivia.XNA.Content\";
+		// mklink /D C:\SimpsonsTrivia.XNA.Content  C:\GitHub\StevePro7\SimpsonsTriviaXNA\SimpsonsTrivia.XNA\SimpsonsTrivia.XNA\bin\x86\Debug\		
+		// mklink /D C:\SimpsonsTrivia.XNA.Content  C:\SVN\SimpsonsTrivia\SimpsonsTrivia.XNA\SimpsonsTrivia.XNA\SimpsonsTrivia.XNA\bin\x86\Debug\
+		protected const String CONTENT_ROOT = @"C:\SimpsonsTrivia.XNA.Content\";
 
 #pragma warning disable 618
 		[TestFixtureSetUp]
